402c58f56093646ab09a61f959ef74450b1af683
Kraig Spear <kraig.spear@dornerworks.com>
2020-01-08 08:42:21 -0500

    Initial Commit

---
+++ HomeKitLights/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeKitLights
+//
+//  Created by Kraig Spear on 1/8/20.
+//  Copyright © 2020 Kraig Spear. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
